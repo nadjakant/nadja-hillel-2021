55be18d5fe7e87884f8bf5bdb9373a81aa9ec5a5
nadjakant <nadjakant@gmail.com>
2022-01-23 12:31:55 +0200

    HW7

--- src/HW6/WholeInfoComparator.java
+++ src/HW6/WholeInfoComparator.java
@@ -2,6 +2,7 @@ package HW6;
 
 import java.util.Comparator;
 public class WholeInfoComparator implements Comparator<Phone>{
+
     @Override
     public int compare(Phone o1, Phone o2) {
         return o1.getWholeInfo().compareTo(o2.getWholeInfo());
